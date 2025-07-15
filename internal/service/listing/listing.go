355602e4bfc61434a1e5fb94894d7ef7f8723bd0
justcgh9 <a.kabardiyadi@innopolis.university>
2025-07-15 17:05:28 +0300

    listing service

---
+++ internal/service/listing/listing.go
@@ -0,0 +1,39 @@
+package listing
+
+import (
+	"context"
+	"errors"
+	"strings"
+
+	"github.com/justcgh9/vk-internship-application/internal/models"
+	"github.com/justcgh9/vk-internship-application/internal/storage"
+)
+
+var (
+	ErrInvalidListing = errors.New("invalid listing data")
+)
+
+type ListingService interface {
+	Create(ctx context.Context, l *models.Listing) (*models.Listing, error)
+	List(ctx context.Context, filter storage.ListFilter) ([]*models.ListingWithAuthor, error)
+}
+
+type service struct {
+	listingRepo storage.ListingRepository
+}
+
+func New(listingRepo storage.ListingRepository) ListingService {
+	return &service{listingRepo: listingRepo}
+}
+
+func (s *service) Create(ctx context.Context, l *models.Listing) (*models.Listing, error) {
+	if strings.TrimSpace(l.Title) == "" || len(l.Title) > 100 ||
+		len(l.Description) > 1000 || l.Price <= 0 || l.UserID == 0 {
+		return nil, ErrInvalidListing
+	}
+	return s.listingRepo.CreateListing(ctx, l)
+}
+
+func (s *service) List(ctx context.Context, filter storage.ListFilter) ([]*models.ListingWithAuthor, error) {
+	return s.listingRepo.ListListings(ctx, filter)
+}
